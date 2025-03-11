033a919cc667c2e441c31f0b83d75d228423aa56
anasteshi <anasteshi13@gmail.com>
2025-03-11 10:45:31 +0200

    Moved repo to folder out of project

---
+++ SimpleCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace SimpleCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Table();
+    }
+
+    static void Table()
+    {
+        string msg = "Тип {0, 8} |\t.NET {1, 8} |\tРозмір = {2} \t|\t MIN = {3, 30}\t| MAX = {4}";
+        Console.Title = "Типи даних у C#";
+        Console.WriteLine("\n\t\t\t\t\tЧИСЛОВІ (цілі) ТИПИ:\n");
+        Console.WriteLine(string.Format(msg, "byte", nameof(Byte), sizeof(byte), byte.MinValue, byte.MaxValue));
+        Console.WriteLine(string.Format(msg, "sbyte", nameof(SByte), sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue));
+        Console.WriteLine(string.Format(msg, "short", nameof(Int16), sizeof(short), short.MinValue, short.MaxValue));
+        Console.WriteLine(string.Format(msg, "ushort", nameof(UInt16), sizeof(ushort), ushort.MinValue, ushort.MaxValue));
+        Console.WriteLine(string.Format(msg, "int", nameof(Int32), sizeof(int), int.MinValue, int.MaxValue));
+        Console.WriteLine(string.Format(msg, "uint", nameof(UInt32), sizeof(uint), uint.MinValue, uint.MaxValue));
+        Console.WriteLine(string.Format(msg, "long", nameof(Int64), sizeof(long), long.MinValue, long.MaxValue));
+        Console.WriteLine(string.Format(msg, "ulong", nameof(UInt64), sizeof(ulong), ulong.MinValue, ulong.MaxValue));
+        
+        Console.WriteLine("\n\t\t\t\t\tЧИСЛОВІ (з плаваючою точкою) ТИПИ:\n");
+        Console.WriteLine(string.Format(msg, "float", nameof(Single), sizeof(float), float.MinValue, float.MaxValue));
+        Console.WriteLine(string.Format(msg, "double", nameof(Double), sizeof(double), double.MinValue, double.MaxValue));
+        Console.WriteLine(string.Format(msg, "decimal", nameof(Decimal), sizeof(decimal), decimal.MinValue, decimal.MaxValue));
+        
+        Console.WriteLine("\n\t\t\t\t\tСИМВОЛЬНІ ТИПИ:\n");
+        Console.WriteLine(string.Format(msg, "char", nameof(Char), sizeof(char), char.MinValue, "?"));
+        Console.WriteLine(string.Format(msg, "string", nameof(String), "N/A", "N/A", "N/A"));
+
+        Console.WriteLine("\n\t\t\t\t\tЛОГІЧНИЙ ТИП:\n");
+        Console.WriteLine(string.Format(msg, "bool", nameof(Boolean), sizeof(bool), "False", "True"));
+        
+        Console.WriteLine("\n\t\t\t\t\tСПЕЦІАЛЬНІ ТИПИ:\n");
+        Console.WriteLine(string.Format(msg, "object", nameof(Object), "N/A", "N/A", "N/A"));
+        Console.WriteLine(string.Format(msg, "dynamic", "N/A", "N/A", "N/A", "N/A"));
+    }
+}
