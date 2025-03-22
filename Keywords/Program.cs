33a16210fca23473aa07237b1589e0e64895f270
anasteshi <anasteshi13@gmail.com>
2025-03-22 11:21:30 +0200

    Added new proj, keyword in

---
+++ Keywords/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+
+namespace Keywords;
+
+class Program
+{
+    static void Foo(ref int value)
+    {
+        value++;
+        Console.WriteLine(value);
+    }
+
+    static void Bar(out int value)
+    {
+        value = 0;
+        value++;
+        Console.WriteLine(value);
+    }
+    
+    
+    //for speed test
+
+    struct MyStruct
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+    }
+    static void Abar(in MyStruct value)
+    {
+    }
+
+    static void Raba(MyStruct value)
+    {
+    }
+    
+
+    static void Main(string[] args)
+    {
+                                    //keyword out and ref differences 
+        
+        
+        // int a = 5;
+        // Foo(ref a);
+        
+        // //int b; 
+        // //Bar(out b);
+        //================
+        // Bar(out int b);
+        
+        // string str = Console.ReadLine();
+        // int.TryParse(str, out int num);
+        // Console.WriteLine(num);
+        //================ 
+        // int.TryParse(Console.ReadLine(), out int number); //if s: cannot be parsed to int, result'll be 0
+        // Console.WriteLine(number);
+        
+                                    //speed test in and w/o in keyword
+                                    
+        MyStruct myStruct = new MyStruct();
+        Stopwatch sw = Stopwatch.StartNew();
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            Abar(myStruct);
+            Console.WriteLine(i+1);
+        }
+        sw.Stop();
+        Console.Clear();
+        Console.WriteLine($"With in keyword: {sw.Elapsed}");
+        Console.ReadKey();
+        sw.Restart();
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            Raba(myStruct);
+            Console.WriteLine(i+1);
+        }
+        sw.Stop();
+        Console.Clear();
+        Console.WriteLine($"Without in keyword: {sw.Elapsed}");
+
+    }
+} 
--- SimpleCode/obj/Debug/net9.0/SimpleCode.AssemblyInfo.cs
+++ SimpleCode/obj/Debug/net9.0/SimpleCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b313d4be6266d0dc909edf61bd075ad51c5aab33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175c8485350e48f55549a93bc2f667131332eecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
