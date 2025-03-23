4d432e9f1b368283dec2dde54c48df876abb2f6b
anasteshi <anasteshi13@gmail.com>
2025-03-23 21:52:15 +0200

    Recursion

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,9 +1,63 @@
 ﻿namespace Recursion;
-
 class Program
 {
+    class Item
+    {
+        public int Value { get; set; }
+        public Item Child { get; set; }
+    }
+
+    static Item GetItem()
+    {
+        return new Item()
+        {
+            Value = 5,
+            Child = new Item()
+            {
+                Value = 6,
+                Child = new Item()
+                {
+                    Value = 7,
+                }
+            }
+
+        };
+    }
+
+    static void Print(in Item item)
+    {
+        // if (item != null)
+        // {
+        //     Console.WriteLine(item.Value);
+        //     Print(item.Child);
+        // }
+        //===============================
+        for (Item i = item; i != null; i = i.Child)
+        {
+            Console.WriteLine(i.Value);
+        }
+    }
+    
+    static void Recursion(int i)
+    {
+        Console.Write(i + " ");
+        i++;
+        if (i >= 5)
+            return;
+        Recursion(i);
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // for (int i = 0; i < 5; i++) //traditional way
+        // {
+        //     Console.Write(i + " ");
+        // }
+        // //==========================================
+        // Console.WriteLine();
+        // Recursion(1);
+        
+        Item item = GetItem();
+        Print(item);
     }
 }
--- Recursion/obj/Debug/net9.0/Recursion.AssemblyInfo.cs
+++ Recursion/obj/Debug/net9.0/Recursion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recursion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc95dbf07da61facd941f3567b610bcfd1c5e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861780f983c151373e1c2be77ecbe3c2ea66c0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recursion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recursion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
